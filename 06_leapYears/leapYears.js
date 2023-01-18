8b12d27f899e674ae0e8067b5e6ef1bd68ab5013
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-01-18 13:55:54 -0500

    feat: implement leapYears, pass tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // first, check if divisible by 4
+    if (year % 4 === 0) {
+        // century years are only leap years if divisible by both
+        // 100 and 400
+        if (year % 100 === 0 && year % 400 === 0) {
+            return true
+        } else if (year % 100 === 0) {
+            return false
+        }
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
