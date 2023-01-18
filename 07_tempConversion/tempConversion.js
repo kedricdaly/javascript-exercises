fcdad3209ad92fd52b8e141aa132ca14a9b4dfda
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-01-18 14:09:16 -0500

    feat: implement tempConversion, pass initial tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const RATIO = 5/9;
+  const OFFSET = -32;
+  
+  return (tempF + OFFSET) * RATIO;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const RATIO = 9/5;
+  OFFSET = 32;
+
+  return (tempC * RATIO) + OFFSET;
+
 };
 
 // Do not edit below this line
