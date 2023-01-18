d59d60d38004cd5183aa6afdf806fec74b89861b
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-01-18 12:50:37 -0500

    feat(sumAll): make inputs order-agnostic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function(lBound,uBound) {
+const sumAll = function(firstN,secondN) {
+    
+    // rearrange inputs so code is order-agnostic
+    let lBound = Math.min(firstN, secondN);
+    let uBound = Math.max(firstN, secondN);
 
-    fSum = 0;
+    let fSum = 0;
     for (let i = lBound; i <= uBound; i++){
         fSum += i;
     }
