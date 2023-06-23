f663d450260d605e8b88466b60cf0f55222dd56a
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-06-23 10:10:19 -0400

    feat: implement palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
-};
+// clean up string
+// https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
+let clean = str.toLowerCase().replace(/[^\w]|_/g,'') // force lowercase and only keep EN-US alphanumeric
+
+let backward = clean.split('').reverse().join('');
+
+return backward == clean;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
