54a3eaadf37a3c5819ffd909f1289584f070e979
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-06-23 11:20:04 -0400

    implement 10_fibonacci (#11)
    * feat: recursive implementation of fibonacci
    
    * feat(fibonacci): implement memoization

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    n = parseInt(n); // allows for strings
 
+    let fibCache = {}
+
+    const memofib = function(n) {
+        if (n in fibCache) return fibCache[n];
+        if (n <= 2) return 1;
+        return fibCache[n] = memofib(n - 1) + memofib(n - 2);
+    }
+    
+    return memofib(n);
 };
 
 // Do not edit below this line
