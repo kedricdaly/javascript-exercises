056197c05bc7e12b174e6e36a7c781b78c2698f8
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-06-23 10:46:32 -0400

    feat: recursive implementation of fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    n = parseInt(n); // allows for strings
+
+    // recursive with no memoization implementation
+    if (n == 1) {return 1} else if (n == 2) {return 1};
+
+    //console.log(fibonacci(n-1) + fibonacci(n - 2))
+    return fibonacci(n - 1) + fibonacci(n - 2);
+
+    
+
 
 };
 
