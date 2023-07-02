d1ef6e0ed225698ecac3aad44828f859298c0358
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-07-01 20:59:45 -0400

    implement 12_findTheOldest (#13)
    * feat(findTheOldest): first implementation
    
    * refactor(findTheOldest): functionalize age calculation

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    return people.reduce((currentOldest, testOldest) => {
+        curOldAge = getAge(currentOldest)
+        testOldAge = getAge(testOldest)
+
+        if      (curOldAge > testOldAge) return currentOldest
+        else if (curOldAge < testOldAge) return testOldest
+        else                             return currentOldest //tie or first iteration
+        }) // no initial value defaults to array[0]
+
+    
+};
+
+// helper function to get age of a person object
+const getAge = function(person) {
+    if (person.yearOfDeath) {
+        deathYear = person.yearOfDeath
+    } else {
+        deathYear = new Date().getFullYear()
+    }
+    return deathYear - person.yearOfBirth
 };
 
 // Do not edit below this line
