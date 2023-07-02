51999993c517a7bbe0358c647e29e7e1ec5fac00
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-07-01 20:54:52 -0400

    refactor(findTheOldest): functionalize age calculation

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,27 +1,25 @@
 const findTheOldest = function(people) {
 
-    // this map function mutates person object and people object
-    people.map(person => {
-        if (person.yearOfDeath) {
-            deathYear = person.yearOfDeath
-        } else {
-            deathYear = new Date().getFullYear()
-        }
-        person.age = deathYear - person.yearOfBirth
-    })
-    return people.reduce((accumulator, currentVal) => {
-        if (accumulator.age > currentVal.age){
-            return accumulator
-        }
-        else if (accumulator.age < currentVal.age) {
-                return currentVal
-            }
-        else {
-            //tie or first iteration
-            return accumulator
-        }
-        }, people[0]
-    )
+    return people.reduce((currentOldest, testOldest) => {
+        curOldAge = getAge(currentOldest)
+        testOldAge = getAge(testOldest)
+
+        if      (curOldAge > testOldAge) return currentOldest
+        else if (curOldAge < testOldAge) return testOldest
+        else                             return currentOldest //tie or first iteration
+        }) // no initial value defaults to array[0]
+
+    
+};
+
+// helper function to get age of a person object
+const getAge = function(person) {
+    if (person.yearOfDeath) {
+        deathYear = person.yearOfDeath
+    } else {
+        deathYear = new Date().getFullYear()
+    }
+    return deathYear - person.yearOfBirth
 };
 
 // Do not edit below this line
