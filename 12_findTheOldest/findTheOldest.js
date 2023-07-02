2020b675d8424b5ddc40cc5000bdf3a31a83419f
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-07-01 20:42:45 -0400

    feat(findTheOldest): first implementation

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // this map function mutates person object and people object
+    people.map(person => {
+        if (person.yearOfDeath) {
+            deathYear = person.yearOfDeath
+        } else {
+            deathYear = new Date().getFullYear()
+        }
+        person.age = deathYear - person.yearOfBirth
+    })
+    return people.reduce((accumulator, currentVal) => {
+        if (accumulator.age > currentVal.age){
+            return accumulator
+        }
+        else if (accumulator.age < currentVal.age) {
+                return currentVal
+            }
+        else {
+            //tie or first iteration
+            return accumulator
+        }
+        }, people[0]
+    )
 };
 
 // Do not edit below this line
